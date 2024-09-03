e41e0ecabee6b5065b12d6e4089fa44c239a78b4
Olivia Burgess <oliviaburgess@protonmail.com>
2024-09-03 22:48:18 +1200

    feat: update modal with intersection type

--- models/leaderboard.ts
+++ models/leaderboard.ts
@@ -1,5 +1,20 @@
+interface LeaderboardEntry {
+  id: number
+  name: string
+}
+
+export interface LeaderboardWin extends LeaderboardEntry {
+  wins: number
+}
+
+export interface LeaderboardLoss extends LeaderboardEntry {
+  losses: number
+}
+
+export type Ratio = LeaderboardWin & LeaderboardLoss
+
 export interface Leaderboard {
-  wins: [{ name: string; wins: number }]
-  losses: [{ name: string; losses: number }]
-  winsAndLossesRatio: [{ name: string; wins: number; losses: number }]
+  wins: LeaderboardWin[]
+  losses: LeaderboardLoss[]
+  winsAndLossesRatio: Ratio[]
 }
