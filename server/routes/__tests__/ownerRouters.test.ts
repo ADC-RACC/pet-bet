da8b643c0ba6415fd9588e06b3b025c9a41767b7
eric-mokuma <171113091+eric-mokuma@users.noreply.github.com>
2024-09-03 16:15:47 +1200

    Test ownerRouters complete

--- server/routes/__tests__/ownerRouters.test.ts
+++ server/routes/__tests__/ownerRouters.test.ts
@@ -1,15 +1,41 @@
-import {
-  expect,
-  it,
-  describe,
-  beforeAll,
-  beforeEach,
-  afterAll,
-  vi,
-  afterEach,
-} from 'vitest'
-import request from 'supertest'
-
+import { describe, it, expect, beforeAll, beforeEach, afterAll } from 'vitest'
 import connection from '../../db/connection.ts'
-import * as db from '../../db/functions/pets.ts'
 import server from '../../server.ts'
+import request from 'supertest'
+import { StatusCodes } from 'http-status-codes'
+
+beforeAll(async () => {
+  await connection.migrate.latest()
+})
+
+beforeEach(async () => {
+  await connection.seed.run()
+})
+
+afterAll(async () => {
+  await connection.destroy()
+})
+
+describe('Getting Pet with their owner Id', () => {
+  it('responds with all the data', async () => {
+    const ownerId = '1'
+    const res = await request(server).get(
+      `/api/v1/owners/${ownerId}/pets?include=true`,
+    )
+
+    console.log('Response status:', res.status)
+    console.log('Response body:', res.body)
+
+    expect(res.status).toBe(StatusCodes.OK)
+    expect(res.body).toStrictEqual([
+      {
+        id: 1,
+        name: 'Sir Whiskers',
+        bio: 'A cat who loves to chase laser pointers and naps on keyboards.',
+        wins: 5,
+        losses: 85,
+        imgUrl: '/images/SirWhiskers.png',
+      },
+    ])
+  })
+})
