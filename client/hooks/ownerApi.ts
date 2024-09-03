56a9e37491b4bc30c1241f7cb411b487ad2a409f
eric-mokuma <171113091+eric-mokuma@users.noreply.github.com>
2024-09-04 10:01:57 +1200

    Owner hook complete

--- client/hooks/ownerApi.ts
+++ client/hooks/ownerApi.ts
@@ -1,10 +1,17 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import request from 'superagent'
-import { Owner } from '../../models/owner'
+import { PetData } from '../../models/pets'
 
-export function useOwnerId(auth0_id: string) {
-  return useQuery<Owner>({
-    queryKey: ['owner', auth0_id],
-    queryFn: async () => {},
+export function useOwnerId(ownerId: string) {
+  return useQuery<PetData>({
+    queryKey: ['pet', ownerId],
+    queryFn: async () => {
+      const res = await request.get(`/api/v1/owners/${ownerId}/pets`)
+      if (!res.body.pet) {
+        throw new Error('Pet not found')
+      }
+      return res.body as PetData
+    },
+    staleTime: 5000,
   })
 }
