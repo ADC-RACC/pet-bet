76b72b0bdc72a9ed51393c0da7dcedadbbaa9f94
Alexander Clark <alexanderclarknz@gmail.com>
2024-09-04 16:16:47 +1200

    feat: Add SubTitle and format lists

--- client/pages/Leaderboard.tsx
+++ client/pages/Leaderboard.tsx
@@ -10,14 +10,13 @@ function Leaderboard() {
   return (
     <>
       <PageTitle title="The Big 5" />
-      <div className="flex">
-        <div>
+      <div className="flex justify-between">
+        <div className="w-72">
           <PageSubTitle
             subtitle="Most "
             subtitleColored="Losses"
             color="text-red"
           />
-          <h2>Losses</h2>
           <ul>
             {leaderboard.losses.map((losses) => (
               <li
@@ -30,10 +29,12 @@ function Leaderboard() {
             ))}
           </ul>
         </div>
-        <div>
-          <h2>
-            Most <span>Even Win/Loss</span>!
-          </h2>
+        <div className="w-72">
+          <PageSubTitle
+            subtitle="Most "
+            subtitleColored="Even Win/Loss"
+            color="text-yellow"
+          />
           <ul>
             {leaderboard.winsAndLossesRatio.map((winLoss) => (
               <li
@@ -41,15 +42,17 @@ function Leaderboard() {
                 className="border-solid border-border border-2"
               >
                 <p>{winLoss.name}.</p>
-                <p>{winLoss.ratio} good/evil ratio.</p>
+                <p>{winLoss.ratio.toFixed(3)} good/evil ratio.</p>
               </li>
             ))}
           </ul>
         </div>
-        <div>
-          <h2>
-            Most <span>Wins</span>!
-          </h2>
+        <div className="w-72">
+          <PageSubTitle
+            subtitle="Most "
+            subtitleColored="Wins"
+            color="text-green"
+          />
           <ul>
             {leaderboard.wins.map((wins) => (
               <li key={wins.id} className="border-solid border-border border-2">
