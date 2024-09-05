be980874c85428a41b09c0741776ee6fcfc2dc1a
edward soung <edward.soung42@gmail.com>
2024-09-06 11:26:47 +1200

    added styling to 404 not found, and working link back to home

--- client/pages/PetProfile.tsx
+++ client/pages/PetProfile.tsx
@@ -13,8 +13,22 @@ function PetProfile() {
     return <p>Error!</p>
   }
   if (data === null) {
-    return 'Hello'
+    return (
+      <div className="text-center">
+        <h1 className="text-6xl font-bold text-red-500">404</h1>
+        <p className="text-2xl mt-4 text-gray-700">
+          Woofsies! The goodest pet you are looking for does not exist.
+        </p>
+        <a
+          href="/"
+          className="mt-6 inline-block px-6 py-3 bg-blue-500 text-black rounded-full hover:bg-blue-600 transition"
+        >
+          Go back home
+        </a>
+      </div>
+    )
   }
+
   return (
     <>
       <div className="flex flex-col md:flex-row items-center space-y-6 md:space-y-0 md:space-x-10">
