4f914fa010597cb10a720a5610c5163d26b92a9d
Alexander Clark <alexanderclarknz@gmail.com>
2024-09-06 10:07:43 +1200

    feat: Add links to 404 page that link back to main pages

--- client/pages/NotFound.tsx
+++ client/pages/NotFound.tsx
@@ -1,11 +1,54 @@
 import PageTitle from '@/components/PageTitle'
-
+import { Link } from 'react-router-dom'
+import PageSubTitle from '@/components/PageSubTitle'
+const links = [
+  { name: 'Random', to: '/random' },
+  { name: 'Leaderboard', to: '/leaderboard' },
+]
 function NotFound() {
   return (
     <>
-      <PageTitle title="Not Found" />
+      <PageTitle title="404 Page Not Found" />
+      <div className="text-center pt-3">
+        <PageSubTitle
+          subtitle="Were you looking for these?"
+          subtitleColored=""
+          color=""
+        />
+      </div>
+      <ul className="hidden flex-1 justify-center text-primary md:flex xl:space-x-1">
+        <li
+          className="-my-0.5 pt-3 pb-3 rounded-sm gradient-p-to-y relative"
+          key={'Home'}
+        >
+          <DesktopLink to={'/'}>{'Home'}</DesktopLink>
+        </li>
+        {links.map((link) => (
+          <li
+            className="-my-0.5 pt-3 pb-3 rounded-sm gradient-p-to-y relative"
+            key={link.to}
+          >
+            <DesktopLink to={link.to}>{link.name}</DesktopLink>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
-
+export function DesktopLink({
+  to,
+  children,
+}: {
+  to: string
+  children: React.ReactNode
+}) {
+  return (
+    <Link
+      to={to}
+      className="rounded-md px-2 py-2 font-header text-base font-bold text-destructive mix-blend-luminosity transition-colors hover:bg-green lg:px-4 lg:text-xl"
+    >
+      {children}
+    </Link>
+  )
+}
 export default NotFound
