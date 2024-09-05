2b6158b42d8fc474688282027df669eb39653c75
Olivia Burgess <oliviaburgess@protonmail.com>
2024-09-06 11:01:19 +1200

    feat: work on button functions and styling

--- client/components/Button.tsx
+++ client/components/Button.tsx
@@ -4,9 +4,17 @@ interface Props {
   variant?: string
   onClick: (event: React.MouseEvent) => void
   children: React.ReactNode
+  textColor?: string
+  border?: string
 }
 
-export function Button({ variant, onClick, children }: Props) {
+export function Button({
+  variant,
+  onClick,
+  children,
+  textColor,
+  border,
+}: Props) {
   let color = ''
   switch (variant) {
     case 'red':
@@ -14,6 +22,9 @@ export function Button({ variant, onClick, children }: Props) {
       break
     case 'green':
       color = 'bg-green'
+      break
+    case 'white':
+      color = 'bg-white'
       break
     case 'hover-red':
       color = 'hover:bg-red bg-secondary'
@@ -25,11 +36,29 @@ export function Button({ variant, onClick, children }: Props) {
       color = 'bg-primary hover:bg-secondary'
   }
 
+  let fontColor = ''
+  switch (textColor) {
+    case 'black':
+      fontColor = 'text-black'
+      break
+    default:
+      fontColor = 'text-white'
+  }
+
+  let borderColor = ''
+  switch (border) {
+    case 'border':
+      borderColor = 'border-solid border-border border-2 border-black'
+      break
+    default:
+      borderColor = ''
+  }
+
   return (
     <button
       onClick={onClick}
       type="button"
-      className={`inline-block rounded px-6 pb-1.5 pt-2 text-sm font-medium uppercase leading-normal text-white  shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out ${color} hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] md:mb-0 md:mr-2`}
+      className={`inline-block rounded px-6 pb-1.5 pt-2 text-sm font-medium uppercase leading-normal ${fontColor} ${borderColor} shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out ${color} hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] md:mb-0 md:mr-2`}
     >
       {children}
     </button>
--- client/pages/AddPet.tsx
+++ client/pages/AddPet.tsx
@@ -6,14 +6,16 @@ import { Button } from '@/components/Button'
 function AddPet() {
   const addNewPet = useAddPet()
 
-  const [formState, setFormState] = useState({
+  const initialFormState = {
     ownerId: '',
     name: '',
     bio: '',
     wins: 0,
     losses: 0,
     imgUrl: '',
-  })
+  }
+
+  const [formState, setFormState] = useState(initialFormState)
 
   const handleChange = (
     evt: ChangeEvent<
@@ -32,31 +34,41 @@ function AddPet() {
     await addNewPet.mutate(formState)
   }
 
+  const handleClear = () => {
+    setFormState(initialFormState)
+  }
+
   return (
     <>
-      <>
-        <PageTitle title="Add A Pet" />
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            onChange={handleChange}
-            value={formState.name}
-          ></input>
-          <label htmlFor="bio">Bio</label>
-          <textarea
-            id="bio"
-            name="bio"
-            onChange={handleChange}
-            value={formState.bio}
-            placeholder={`Tell us a little bit about ${formState.name}`}
-            rows={4}
-          ></textarea>
-          <Button>Submit</Button>
-        </form>
-      </>
+      <PageTitle title="Add A Pet" />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={handleChange}
+          value={formState.name}
+        ></input>
+        <label htmlFor="bio">Bio</label>
+        <textarea
+          id="bio"
+          name="bio"
+          onChange={handleChange}
+          value={formState.bio}
+          placeholder={`Tell us a little bit about ${formState.name}`}
+          rows={4}
+        ></textarea>
+        <Button onClick={handleSubmit}>Add</Button>
+        <Button
+          onClick={handleClear}
+          variant="white"
+          textColor="black"
+          border="border"
+        >
+          Clear
+        </Button>
+      </form>
     </>
   )
 }
