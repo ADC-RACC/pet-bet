3ed3cd7e1d7fce69739676557ba901ecd2b63d78
Olivia Burgess <oliviaburgess@protonmail.com>
2024-09-05 14:22:37 +1200

    feat: change margin on list items

--- client/components/LeaderboardColumn.tsx
+++ client/components/LeaderboardColumn.tsx
@@ -20,9 +20,9 @@ export default function LeaderboardColumn({
       {column.map((item) => (
         <li
           key={item.id}
-          className="border-solid border-border border-2 pt-3 pb-3 pl-3 rounded-sm"
+          className="-my-0.5 border-solid border-border border-2 pt-3 pb-3 pl-3 rounded-sm"
         >
-          <p className="font-bold text-xl">{item.name}</p>
+          <p className="font-bold m-0 text-xl">{item.name}</p>
           <p>
             {dataType === 'ratio' ? item[dataType].toFixed(3) : item[dataType]}{' '}
             {description}
